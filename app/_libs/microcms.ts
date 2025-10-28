40f08233eec477d785340540b2a7f3ea17d7744b
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-10-28 11:25:00 +0900

    下書きプレビュー時はSSRをする

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -61,6 +61,9 @@ export const getNewsDetail = async (
     endpoint: "news",
     contentId,
     queries,
+    customRequestInit: {
+      next: { revalidate: queries?.draftKey === undefined ? 60 : 0 },
+    },
   });
   return detailData;
 };
